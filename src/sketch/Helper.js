01c0a295de6fc7ecc0dc8f557b367095c5316289
scammi <scammi@gmail.com>
2020-09-30 19:24:11 -0300

    simpler sketch

--- src/sketch/Helper.js
+++ src/sketch/Helper.js
@@ -1,4 +1,5 @@
 import { AutoCell, DeadCell, FastCell } from './AltCell.js';
+import { Cell } from './Cell';
 
 export class Helper {
 
@@ -7,7 +8,7 @@ export class Helper {
     let AltCellBtn = window.global.AltCellBtn;
     let size = window.global.size;
     let new_cell;
-    
+
     switch (AltCellBtn) {
       case 'Auto':
         new_cell = new AutoCell(this.colPosition * size, this.rowPosition * size, size, this.colPosition, this.rowPosition);
@@ -18,15 +19,17 @@ export class Helper {
       case 'Fast':
         new_cell = new FastCell(this.colPosition * size, this.rowPosition * size, size, this.colPosition, this.rowPosition);
         break;
+      case 'Normal':
+        new_cell = new Cell(this.colPosition * size, this.rowPosition * size, size, this.colPosition, this.rowPosition);
+        break;
     }
 
     tissue.setCell(this.colPosition, this.rowPosition, new_cell);
   }
 
-  static which_cell() {
-
-
-
-
+  static record_data_for_graph(){
+    if (this.colPosition == 12 && this.rowPosition == 12) {
+      window.global.request_data.data.push(this.Vm);
+    } 
   }
 }
--- src/sketch/sketch.js
+++ src/sketch/sketch.js
@@ -7,30 +7,18 @@ export default( sketch ) => {
   let cols = window.global.cols;
   let size = window.global.size;
   
-  function paint(cell) {
-    sketch.rect(cell.x, cell.y, cell.width, cell.height);
-    sketch.fill(cell.stateColor());
-  }
-
-  sketch.setup = () => {  
-    let cvn_height = (size * (cols - 2));
-    let cvn_width = (size * (rows-2));
-
-    sketch.createCanvas(cvn_height, cvn_width);
-
-    window.global.tissue = new Tissue(cols, rows);
-    window.global.tissue.refreshAllReference();
+  sketch.setup = () => { 
+    create_canvas(sketch)
+    create_grid()
   };
 
   sketch.draw = () => {
-    sketch.background(0);
-
-    //Shifts canvas to remove 2top rows and col.
-    sketch.translate((size * -1), (size * -2))
-
+    
     let tissue = window.global.tissue; 
     let play = window.global.play;
     let stop = window.global.stop;
+    
+    sketch.translate((size * -1), (size * -2))  //Shifts canvas to remove 2top rows and col.
     
     if (play == true) {
       tissue.forAll( function() { this.membranePotential() } );
@@ -48,16 +36,31 @@ export default( sketch ) => {
     tissue.forAll( function() { 
       paint(this);
 
-      if (this.colPosition == 12 && this.rowPosition == 12){
-        window.global.request_data.data.push(this.Vm);
-      }
+      Helper.record_data_for_graph.call(this)
 
       if (this.isInSide(sketch.mouseX, sketch.mouseY)) {
-        console.log("State", this.state ,"Coord", this.colPosition,", ", this.rowPosition, " Vm=", this.Vm);
-      
+        console.log(`State: ${this.state} Coord: ${this.colPosition}, ${this.rowPosition}  Vm: ${this.Vm}`);
         if (sketch.mouseIsPressed) Helper.transaform_cell.call(this); 
       }
-    } );    
+    });
   };
+
+  function paint(cell) {
+    sketch.rect(cell.x, cell.y, cell.width, cell.height);
+    sketch.fill(cell.stateColor());
+  }
+
+  function create_canvas(sketch){
+    let cvn_height = (size * (cols - 2));
+    let cvn_width = (size * (rows-2));
+
+    sketch.createCanvas(cvn_height, cvn_width);
+  }
+
+  function create_grid(){
+    window.global.tissue = new Tissue(cols, rows);
+    window.global.tissue.refreshAllReference();
+  }
+
 };
 

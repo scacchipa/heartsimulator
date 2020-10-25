21547137d237bad732dc59fe063f634cee4a93de
scammi <scammi@gmail.com>
2020-10-25 19:37:36 -0300

    #51 use docs

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,11 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
-   devServer: {
-     contentBase: './dist',
-     port: 9000
-   }
+  output: {
+    path: __dirname + '/docs'
+  },
+  devServer: {
+    contentBase: './docs',
+    port: 9000
+  }
 };
